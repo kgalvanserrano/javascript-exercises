1406fa05938f34038dd49c082362a7791955a949
Kevin <kevin.galvanserrano@gmail.com>
2023-01-13 02:46:56 -0800

    Commiting first 3 questions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let getRepeatedString = "";
+    let error = "ERROR";
 
+    if (num < 0) {
+        return error;
+    }
+
+    while (num > 0) {
+        getRepeatedString += string;
+        num--;
+    }
+    return getRepeatedString
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
